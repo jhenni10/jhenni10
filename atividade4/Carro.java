4c5f2d6d432282b372c3c8f947784996d11c4452
Jhennifer <jhenniferdossantos567@gmail.com>
2025-06-07 21:28:13 -0400

    Atualização da classe carro

---
+++ atividade4/Carro.java
@@ -0,0 +1,10 @@
+public class Carro extends Veiculo implements Motorizado {
+    public Carro(String modelo, int ano){
+        super(modelo,ano);
+    }
+    
+    @Override
+    public void abastecer(){
+        System.out.println("Carro "+ modelo +" está sendo abastecido.");
+    }
+}
