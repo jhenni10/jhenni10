a5cf6422b0e0ea090594469a5e5e03f953dde84e
Jhennifer <jhenniferdossantos567@gmail.com>
2025-06-07 21:26:43 -0400

    Atualização da classe moto

---
+++ atividade4/Moto.java
@@ -0,0 +1,10 @@
+public class Moto extends Veiculo implements Motorizado {
+    public Moto(String modelo, int ano) {
+        super(modelo, ano);
+    }
+
+    @Override
+    public void abastecer() {
+        System.out.println("Moto " + modelo + " está sendo abastecida.");
+    }
+}
