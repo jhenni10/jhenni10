d6cb7f67fed01935c0257a44bf586cd52b3bfd2d
Jhennifer <jhenniferdossantos567@gmail.com>
2025-06-07 21:13:01 -0400

    Atualização das classes

---
+++ atv3/Peixe.java
@@ -0,0 +1,14 @@
+public class Peixe extends Animal implements Aquatico {
+    @Override
+    public void emitirSom() {
+        System.out.println("GLUB GLUB");
+    }
+    @Override
+    public void alimentar() {
+        System.out.println("O peixe está se alimentando de algas.");
+    }
+    @Override
+    public void nadar() {
+        System.out.println("O peixe está nadando.");
+    }
+}
